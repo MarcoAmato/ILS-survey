40db0a495fd000e120a47b075d9906eb8eeb2427
Marco Amato <amatomarco111@gmail.com>
2021-04-21 11:48:43 +0200

    using DataFrame.sample to select lists of movies

--- list_creator.py
+++ list_creator.py
@@ -9,8 +9,11 @@ COLUMNS_SIMILARITY = {'Title:LEV', 'Title:JW', 'Title:LCS', 'Title:BI',
                       'Image:COL', 'Image:EN', 'Plot:LDA', 'Plot:cos', 'Genre:LDA',
                       'Genre:Jacc', 'Stars:Jacc', 'Directors:Jacc', 'Date:MD', 'Tag:Genome',
                       'SVD'}
+MOVIES_LIST_LENGTH = 10
 
 
+# returns the dataset where the columns are only: the two movies id and the similarity measurements.
+# 'num_rows' is the number of similarities we are putting into the dataframe
 def get_database_clean(num_rows: int):
     #  returns a pandas dataframe containing the columns [validation$r1, validation$r2] hence, the film ids, and the
     #  similarity measurements of the two
@@ -18,14 +21,13 @@ def get_database_clean(num_rows: int):
     return pd.read_csv(PATH_TO_RATINGS, nrows=num_rows, sep="\t", usecols=interested_columns)
 
 
-def get_columns_of_dataframe(dataframe: pd.DataFrame):
-    return dataframe.columns
-
-
-def get_similarity_rows_of_movie(dataframe: pd.DataFrame, movie_id: str):
+# returns all the rows of the 'dataframe' where 'movie' is compared to another movie
+def get_similarity_rows_of_movie(dataframe: pd.DataFrame, movie: str):
+    # create empty dataframe with same structure as 'dataframe'
     sim_df_of_movie = pandas.DataFrame(columns=dataframe.columns)
     for i, sim_row in dataframe.iterrows():
-        if sim_row["validation$r1"] == movie_id or sim_row["validation$r2"] == movie_id:
+        # if 'movie' is in the row this is a similarity measure for 'movie'
+        if sim_row["validation$r1"] == movie or sim_row["validation$r2"] == movie:
             sim_df_of_movie = sim_df_of_movie.append(sim_row, ignore_index=True)
     return sim_df_of_movie
 
@@ -51,14 +53,19 @@ def get_mean_similarity(similarity_row: pd.Series):
 
 
 if __name__ == '__main__':
-    similarity_dataframe = get_database_clean(3)
-    mean_similarities = list()
+    similarity_dataframe = get_database_clean(50)
+
+    list_of_movies = similarity_dataframe.sample(MOVIES_LIST_LENGTH)
+
+    print(list_of_movies)
+
+    """mean_similarities = list()
     for index in range(len(similarity_dataframe.index)):
         mean_similarities.append(get_mean_similarity(similarity_dataframe.iloc[index]))
     similarity_dataframe['mean_similarity'] = mean_similarities
 
     movie_id = (similarity_dataframe.iloc[1]["validation$r2"])
-    print(get_similarity_rows_of_movie(similarity_dataframe, movie_id))
+    print(get_similarity_rows_of_movie(similarity_dataframe, movie_id))"""
 
     # print(get_columns_of_dataframe(df))
     # test_validation1 = df.iloc[1]["validation$r1"]
