42456c4b93f32754a6bb956eb4a34e3652ad36fb
Marco Amato <amatomarco111@gmail.com>
2021-04-24 10:54:26 +0200

    Commented save_top_n_movies_by_popularity

---
+++ pre_computation.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+
+def save_top_n_movies_by_popularity(similarities_df: pd.DataFrame, n: int, path_to_save: str) -> None:
+    """
+    The similarity measurements of the top n movies of similarities_df are saved to the path_to_save
+    :param similarities_df: dataframe of movie similarity measurements
+    :param n: number of movies to select
+    :param path_to_save: path where the similarities will be saved
+    """
+    print(f"saving top {n} movies by popularity")
+
+
+
