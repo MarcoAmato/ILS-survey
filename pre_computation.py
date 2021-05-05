183de569ebf864a46be4312ee9e6416eab5f5964
Marco Amato <amatomarco111@gmail.com>
2021-05-05 18:29:47 +0200

    Optimized imports

--- pre_computation.py
+++ pre_computation.py
@@ -1,13 +1,12 @@
-from typing import Dict, List, Set
 from shutil import copyfile
+from typing import Dict, List, Set
 
-import pandas as pd
 from pandas import DataFrame, Series
 
 from main import get_dataframe_movie_ids_and_similarities, get_mean_similarity, get_movies_from_df, \
     COLUMNS_SIMILARITY, get_movie, get_similarity_dataframe, read_movie_ids_from_csv, PATH_TO_ALL_MOVIES_ID, \
-    PATH_TO_SIMILARITY_MEAN, PATH_TO_TOP_10_MOVIES_ID, PATH_TO_SIMILARITY_MPG, PATH_TO_TOP_100_MOVIES_ID, \
-    get_similarities_of_movies, PATH_TO_SIM_100_MPG, PATH_TO_TOP_100_MOVIES_JSON, PATH_TO_TOP_100_MOVIES_JSON
+    PATH_TO_SIMILARITY_MPG, PATH_TO_TOP_100_MOVIES_ID, \
+    get_similarities_of_movies, PATH_TO_SIM_100_MPG, PATH_TO_TOP_100_MOVIES_JSON
 
 COLUMNS_MEAN: Set[str] = {"similarity", "validation$r1", "validation$r2"}
 
